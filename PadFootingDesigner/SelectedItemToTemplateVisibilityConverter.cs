15f30d319a0f5ba0dc16b7204306c89ecec8ac0c
brahma96 <brahma1996@yahoo.com>
2023-04-21 16:41:49 +0800

    Add project files.

---
+++ PadFootingDesigner/SelectedItemToTemplateVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PadFootingDesigner
+{
+    public class SelectedItemToTemplateVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool chosenDataTemplate = (bool)value;
+            //var groupBox = (GroupBox)parameter;
+            Visibility result = Visibility.Collapsed;
+
+            if (chosenDataTemplate)
+            {
+                result = Visibility.Visible;
+            }
+            else
+            {
+                result = Visibility.Collapsed;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
